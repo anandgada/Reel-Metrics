18bdfc7a2a9eefacfd072057beb5a738331c0eac
anandgada <59159455+anandgada@users.noreply.github.com>
2025-05-19 01:35:47 +0530

    Update Header to show export

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { exportData } from "../services/api";
 
 const Header = () => {
   return (
@@ -9,13 +9,14 @@ const Header = () => {
             <h1 className="text-2xl font-bold text-gray-900">
               Users Reels Data
             </h1>
-            <p className="text-sm text-gray-500">
-             Recent 10 reels data
-            </p>
+            <p className="text-sm text-gray-500">Recent 10 reels data</p>
           </div>
-          {/* <div className="bg-blue-50 px-4 py-2 rounded-md">
-            <p className="text-xs text-blue-700 font-medium">Environment: {import.meta.env.VITE_APP_ENV || 'Development'}</p>
-          </div> */}
+          <div
+            onClick={exportData}
+            className="bg-blue-50 px-4 py-2 rounded-md select-none cursor-pointer text-xs"
+          >
+            Export
+          </div>
         </div>
       </div>
     </header>
